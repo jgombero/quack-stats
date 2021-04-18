42e170ea5a1565e97584d57ac35ff0a94e00a5e9
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-18 15:59:54 -0700

    Create and export duckForm constants

---
+++ client/src/components/DuckForm/constants/constants.js
@@ -0,0 +1,12 @@
+export const timeNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+export const timePeriods = ["am", "pm"];
+
+export const defaultState = {
+  timeNumber: "",
+  timePeriod: "",
+  food: "",
+  park: "",
+  location: "",
+  duckQuantity: 0,
+  foodQuantity: 0,
+};
