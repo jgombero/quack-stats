b4520ee7262c502f21f9916dbde7c3094ee2a016
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-18 13:16:11 -0700

    Render basic form layout

--- client/src/components/DuckForm/DuckForm.js
+++ client/src/components/DuckForm/DuckForm.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Jumbotron, Container, Form, Col, Button } from "react-bootstrap";
 
 const DuckForm = () => {
+  const timeNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+  const timePeriods = ["am", "pm"];
+
   const defaultState = {
     time: "",
     food: "",
@@ -50,7 +54,71 @@ const DuckForm = () => {
       });
   };
 
-  return <h1>This is the Duck Form.</h1>;
+  return (
+    <>
+      <Jumbotron>
+        <Container>
+          <h1>Quack Form</h1>
+          <h5>Please enter your duck data.</h5>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <Form>
+          <Form.Label>Time</Form.Label>
+          <Form.Row>
+            <Form.Group as={Col} md="2" controlId="formGridTimeNumber">
+              <Form.Control as="select" required>
+                {timeNumbers.map((timeNumber) => {
+                  return <option>{timeNumber}</option>;
+                })}
+              </Form.Control>
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="2" controlId="formGridTimePeriod">
+              <Form.Control as="select" required>
+                {timePeriods.map((timePeriod) => {
+                  return <option>{timePeriod}</option>;
+                })}
+              </Form.Control>
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} md="4" controlId="formGridFood">
+              <Form.Label>Food</Form.Label>
+              <Form.Control required type="text" placeholder="Ex. Bread" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="formGridFoodQuantity">
+              <Form.Label>Quantity</Form.Label>
+              <Form.Control require type="number" placeholder="0" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} md="4" controlId="formGridParkName">
+              <Form.Label>Park Name</Form.Label>
+              <Form.Control required type="text" placeholder="Ex. Queen Elizabeth" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="formGridParkLocation">
+              <Form.Label>Location</Form.Label>
+              <Form.Control required type="text" placeholder="Ex. Vancouver, BC" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} md="2" controlId="formGridDuckQuantity">
+              <Form.Label>Duck Count</Form.Label>
+              <Form.Control required type="number" />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+          <Button type="submit">Submit form</Button>
+        </Form>
+      </Container>
+    </>
+  );
 };
 
 export default DuckForm;
