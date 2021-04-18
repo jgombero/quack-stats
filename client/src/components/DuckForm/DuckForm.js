3da37af0196bd0d66091b6cfb3598ab33e555180
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-17 20:13:09 -0700

    Create boilerplate component

---
+++ client/src/components/DuckForm/DuckForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DuckForm = () => {
+  return <h1>This is the Duck Form.</h1>;
+};
+
+export default DuckForm;
---
+++ client/src/components/DuckStats/DuckStats.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DuckStats = () => {
+  return <h1>This is the Duck Stats.</h1>;
+};
+
+export default DuckStats;
