fbbcee9d12c387bcd6fad9dfe590f944dc5b3fab
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-17 20:12:21 -0700

    Import Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./components/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar/Navbar";
 import DuckStats from "./components/DuckStats/DuckStats";
 import DuckForm from "./components/DuckForm/DuckForm";
 
