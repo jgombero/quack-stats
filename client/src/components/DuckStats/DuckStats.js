e050324d6459b4ea29e68a07d3c20945cc2305b9
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-17 23:13:38 -0700

    Add, fetch, and set state

--- client/src/components/DuckStats/DuckStats.js
+++ client/src/components/DuckStats/DuckStats.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const DuckStats = () => {
+  const [state, setState] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get("http://localhost:8000/ducks")
+      .then((res) => {
+        setState(...res);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        // TODO: Handle error better
+        console.error(`Error: ${error}`);
+      });
+  }, []);
+
   return <h1>This is the Duck Stats.</h1>;
 };
 
