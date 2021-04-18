f68ea061bd5c187d155b84313dad2a09b4d4031b
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-17 22:09:45 -0700

    Fix navbar name due to bootsrap naming convention

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./components/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar/Navbar";
+import NavigationBar from "./components/Navbar/Navbar";
 import DuckStats from "./components/DuckStats/DuckStats";
 import DuckForm from "./components/DuckForm/DuckForm";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <NavigationBar />
       <h1>Hello World!</h1>
       <Route path="/" exact component={DuckForm} />
       <Route path="/ducks" exact component={DuckStats} />
