3db5e834452184d84dc3cd5e3a2f1ad33067acaa
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-18 18:43:57 -0700

    Render material table with data from database

--- client/src/components/DuckStats/DuckStats.js
+++ client/src/components/DuckStats/DuckStats.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Container, Jumbotron } from "react-bootstrap";
+import MaterialTable from "material-table";
+import { tableIcons, columns } from "./constants/constants";
 
 const DuckStats = () => {
   const [state, setState] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
   useEffect(() => {
     setIsLoading(true);
     axios
       .get("http://localhost:8000/ducks")
       .then((res) => {
-        setState(...res);
+        setState([...res.data]);
         setIsLoading(false);
       })
       .catch((error) => {
@@ -19,7 +21,26 @@ const DuckStats = () => {
       });
   }, []);
 
-  return <h1>This is the Duck Stats.</h1>;
+  return (
+    <>
+      <Jumbotron>
+        <Container>
+          <h1>Quack Stats</h1>
+          <h5>Here are the latest statistics about duck feedings around the world!</h5>
+        </Container>
+      </Jumbotron>
+      <Container>
+        {state.length > 0 && (
+          <MaterialTable
+            title="Duck Feeding Stats"
+            columns={columns}
+            data={state}
+            icons={tableIcons}
+          />
+        )}
+      </Container>
+    </>
+  );
 };
 
 export default DuckStats;
