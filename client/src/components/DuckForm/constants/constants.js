3014804928877ee4df235ef4ef22a7767d5abd1d
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-18 18:43:36 -0700

    Add form constants

--- client/src/components/DuckForm/constants/constants.js
+++ client/src/components/DuckForm/constants/constants.js
@@ -1,9 +1,11 @@
-export const timeNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+export const timeFirstNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+export const timeSecondNumbers = ["00", "15", "30", "45"];
 export const timePeriods = ["am", "pm"];
 
 export const defaultState = {
-  timeNumber: "",
-  timePeriod: "",
+  timeFirstNumber: "12",
+  timeSecondNumber: "00",
+  timePeriod: "pm",
   food: "",
   park: "",
   location: "",
